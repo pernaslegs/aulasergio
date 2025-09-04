eeaca84421236554bc170f68b54ed718bc5d90f2
pernaslegs <166957618+pernaslegs@users.noreply.github.com>
2025-09-04 22:56:17 +0000

    Foram adicionados mais valores a categoria

--- projeto/projetosergio/src/main/resources/import.sql
+++ projeto/projetosergio/src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into Categoria (nome) values ('Bebidas');
 insert into Categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('Padaria');
+insert into Categoria (nome) values ('Doce');
+insert into Categoria (nome) values ('Salagado');
+insert into Categoria (nome) values ('Produtos de limpeza');
