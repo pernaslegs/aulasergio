cce070b787e9134bf70920f25fd2555d719fc574
pernaslegs <166957618+pernaslegs@users.noreply.github.com>
2025-09-17 23:28:16 +0000

    inserido testes

--- projeto/projetosergio/src/main/java/entidades/Produto.java
+++ projeto/projetosergio/src/main/java/entidades/Produto.java
@@ -1,7 +1,6 @@
 package entidades;
-
 import java.time.LocalDate;
-
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Produto {
+public class Produto extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
---
+++ projeto/projetosergio/src/test/java/entidades/ProdutoTests.java
@@ -0,0 +1,26 @@
+package entidades;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.transaction.Transactional;
+
+@QuarkusTest
+public class ProdutoTests {
+    @Test
+    @Transactional
+    public void testBuscaUmaCategoriaBuscaUmFabricanteSalvaUmProduto() {
+        Produto produto = new Produto ();
+        produto.nome = "Fanta Laranja 2 Litros";
+        produto.preco = 9.45;
+        produto.quantidade = 50;
+        produto.validade = LocalDate.of(2025, 12, 20);
+
+        produto.categoria = Categoria.findById(1);
+        produto.fabricante = Fabricante.findById(1);
+
+        produto.persist();
+
+        Assertions.assertEquals(11, produto.codigo);
+    }
+}
